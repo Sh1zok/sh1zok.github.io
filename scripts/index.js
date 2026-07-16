eb7b9f46c96d7ffbbf68db1b51fe783a5ec4a849
Sh1zok <borovojarsenij218@gmail.com>
2026-07-16 20:29:32 +0300

    Got Damn

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,52 @@
-const WelcomeSplashes = [
-    "Hey there! I’m <a id='Sh1zok'>Sh1zok</a>!",
-    "Greetings! My name is <a id='Sh1zok'>Sh1zok</a>!",
-    "Yo! <a id='Sh1zok'>Sh1zok</a> is here!",
-    "Hello! Call me <a id='Sh1zok'>Sh1zok</a>.",
-    "Howdy! Ny name is <a id='Sh1zok'>Sh1zok</a>!",
-    "Welcome, my name is <a id='Sh1zok'>Sh1zok</a>!",
-    "Hiya! <a id='Sh1zok'>Sh1zok</a> is my name!",
-    "Nice to see you! I’m <a id='Sh1zok'>Sh1zok</a>!",
-    "Sup! I go by <a id='Sh1zok'>Sh1zok</a>!",
-    "Hi there, my nickname is <a id='Sh1zok'>Sh1zok</a>!",
-    "Welcome aboard! I’m <a id='Sh1zok'>Sh1zok</a>!",
-    "Cheers! My name is <a id='Sh1zok'>Sh1zok</a>!",
-    "What's up? I'm <a id='Sh1zok'>Sh1zok</a>!",
-    "I'm <a id='Sh1zok'>Sh1zok</a>. That's my website.",
-    "<a id='Sh1zok'>Sh1zok</a> was here.",
-    "I'm <a id='Sh1zok'>Sh1zok</a> UwU"
-]
+import { ruCodes, getCommonLocaleCode, setLocale } from './shared/localeUtils.js';
+import { ruLocaleMap, enLocaleMap } from "./locales/index.js";
+
+function regenerateLocale(localeMap) {
+    setLocale(localeMap).then(() => {
+        const today = new Date();
+        const dateOfBirth = new Date("2006-09-04");
+
+        let years = today.getFullYear() - dateOfBirth.getFullYear();
+        const deltaMonth = today.getMonth() - dateOfBirth.getMonth();
+        if (deltaMonth < 0 || (deltaMonth === 0 && today.getDate() < dateOfBirth.getDate())) {
+            years--;
+        }
+
+        document.getElementById("years-old").textContent = years.toString();
+    })
+
+    const welcomeSplashes = localeMap["welcomeSplashes"]
+    document.getElementById("welcomeSplash").innerHTML = welcomeSplashes[Math.round(Math.random() * (welcomeSplashes.length - 1))];
+
+    const randomSplashes = localeMap["randomSplashes"]
+    document.getElementById("randomThingContent").innerHTML = randomSplashes[Math.round(Math.random() * (randomSplashes.length - 1))];
+
+    const currentYear = new Date().getFullYear();
+    document.getElementById("this-year").textContent = currentYear.toString();
+}
+
+
+
+let preferredLanguage = localStorage.getItem('preferredLanguage');
+if (!preferredLanguage) {
+    preferredLanguage = getCommonLocaleCode(ruCodes, 'ru', 'en');
+    localStorage.setItem('preferredLanguage', preferredLanguage);
+}
+
+let localeMap = (preferredLanguage === 'ru') ? ruLocaleMap : enLocaleMap;
+regenerateLocale(localeMap);
+
+
+
+document.getElementById("changeLangToRus").addEventListener('click', function() {
+    if (localStorage.getItem('preferredLanguage') === "ru") {return}
+
+    localStorage.setItem('preferredLanguage', "ru");
+    regenerateLocale(ruLocaleMap);
+});
+document.getElementById("changeLangToEng").addEventListener('click', function() {
+    if (localStorage.getItem('preferredLanguage') === "en") {return}
+
+    localStorage.setItem('preferredLanguage', "en");
+    regenerateLocale(enLocaleMap);
+});
